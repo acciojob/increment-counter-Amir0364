6f19cd3ecc39cbb5ce5b382a5b44635693c18f42
Mohd Amir <140543590+Amir0364@users.noreply.github.com>
2024-07-06 13:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+var counterElement = document.getElementById('counter');
+var button = document.getElementById('incrementBtn');
+
+var counter = 0;
+counterElement.innerHTML = counter;
+
+button.addEventListener('click', function() {
+	counter++;
+	alert(counter - 1);
+	counterElement.innerHTML = counter;
+	
+});
